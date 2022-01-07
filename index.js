c16afcf9e05f59b5a8d0eedcf800b38f162b278b
Remxin <88281845+Remxin@users.noreply.github.com>
2022-01-07 08:29:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku!</h1>")
+   res.send({ imie: "Dawid", nazwisko: "Nabaglo", klasa: "3i1", grupa: "2" })
 })
 
 app.listen(PORT, function () {
